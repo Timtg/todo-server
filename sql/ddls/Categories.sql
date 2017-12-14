09ee1ec76ef9948f9f56d6e64a117c815de83b0b
Tim Gundersen <tim.gundersen@protectorforsikring.no>
2017-12-14 15:19:23 +0100

    db update

--- sql/ddls/Categories.sql
+++ sql/ddls/Categories.sql
@@ -1,8 +1,8 @@
-CREATE TABLE public.Categories
+CREATE TABLE public.category
 (
     id SERIAL PRIMARY KEY NOT NULL,
     Description TEXT NOT NULL,
     Deleted BOOLEAN NOT NULL,
     Created_Date TIMESTAMP NOT NULL
 );
-CREATE UNIQUE INDEX Categories_id_uindex ON public.Categories (id);
+CREATE UNIQUE INDEX Categories_id_uindex ON public.category (id);
--- sql/ddls/todo_elemement_category.sql
+++ sql/ddls/todo_elemement_category.sql
@@ -3,5 +3,5 @@ CREATE TABLE public.Todo_element_category
     todo_element_id INT NOT NULL,
     category_id INT NOT NULL,
     CONSTRAINT Todo_element_category_todo_body_id_fk FOREIGN KEY (todo_element_id) REFERENCES todo_body (id),
-    CONSTRAINT Todo_element_category_category_id_fk FOREIGN KEY (category_id) REFERENCES categories (id)
+    CONSTRAINT Todo_element_category_category_id_fk FOREIGN KEY (category_id) REFERENCES category (id)
 );
